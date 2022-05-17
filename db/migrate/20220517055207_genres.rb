b429af84a163c3ea04862fd60abf0fba307b80a9
kawa-sho <syogo.wao@gmail.com>
2022-05-17 06:53:18 +0000

    Add model genre cart_item

--- db/migrate/20220517055207_genres.rb
+++
@@ -1,6 +0,0 @@
-class Genres < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :genres
-    drop_table :cart_items
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_055207) do
+ActiveRecord::Schema.define(version: 2022_05_17_045356) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 2022_05_17_055207) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "customer_id"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -77,6 +85,12 @@ ActiveRecord::Schema.define(version: 2022_05_17_055207) do
     t.string "name"
     t.string "postal_code"
     t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
