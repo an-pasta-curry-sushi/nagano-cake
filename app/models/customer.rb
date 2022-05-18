b30806b62a0914aa78cb2bb5dbc559c915b97dcb
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-18 08:33:24 +0000

    [Update] customers_controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  varidates :last_name, length: { minimum: 2, maximum: 20 }
+  varidates :first_name, length: { minimum: 2, maximum: 20 }
+  varidates :last_name_easy, length: { minimum: 2, maximum: 20 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  varidates :first_name_easy, length: { minimum: 2, maximum: 20 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  varidates :phone_number, uniqueness: true
 end
