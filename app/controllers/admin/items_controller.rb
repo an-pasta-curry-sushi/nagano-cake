687e5ae6189e32cc84d250eda6cd7524b60ad837
kawa-sho <syogo.wao@gmail.com>
2022-05-18 06:18:46 +0000

    Add アソシエーション コントローラー内

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,42 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
   end
-
-  def create
-  end
-
+  
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
-
-  def show
-  end
-
+  
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path
+    else
+      render "edit"
+    end
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :item_image, :no_tax_price, :salling_status, :genre_id)
   end
 end
