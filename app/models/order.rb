d7a091db125e7e3c4b38aa79ea26b25dac4470a3
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-19 07:11:21 +0000

    admin/order views[:show]

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,9 @@ class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
 
- enum payment_method: { credit_card: 0, transfer: 1 }
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  def sum_of_price
+    order_detail.price * order_detail.amount
+  end
 end
