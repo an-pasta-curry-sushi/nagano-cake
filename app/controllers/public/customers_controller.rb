feaaa2062ee2322d6fac527c2b9e57ea7a1d8957
kawa-sho <syogo.wao@gmail.com>
2022-05-19 06:16:35 +0000

    Add 会員側　カスタマー関連

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,7 +4,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-   
+    @customer = current_customer
   end
 
   def update
@@ -17,8 +17,25 @@ class Public::CustomersController < ApplicationController
   end
 
   def quit_check
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    if @customer.update(withdraw_params)
+      redirect_to root_path
+    else
+      render "quit_check"
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_easy, :first_name_easy, :postal_code, :address, :phone_number, :email)
+  end
+
+  def withdraw_params
+    params.require(:customer).permit(:is_active)
   end
 end
