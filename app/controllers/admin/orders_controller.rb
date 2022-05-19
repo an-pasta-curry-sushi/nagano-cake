768afc1064ac8773bbd0ee59b3cd24ae8f416c3c
EC2 Default User <kkisobe1213@icloud.com>
2022-05-19 04:43:41 +0000

    Update admin/orders_controller, admin/order_details_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,28 @@
 class Admin::OrdersController < ApplicationController
+  
   def index
+    
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
   
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to request.referer
+    else
+      @order_details = @order.order_details
+      render 'show'
+    end
+  end
+  
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,9 @@ class Customer < ApplicationRecord
   validates :last_name_easy, length: { minimum: 2, maximum: 20 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :first_name_easy, length: { minimum: 2, maximum: 20 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :phone_number, uniqueness: true
+  
+  has_many :deliveries
+  has_many :orders
+  has_many :cart_items
+  
 end
