c0edcdd9dd332030e17c22dfb87ac630f34663ab
kawa-sho <syogo.wao@gmail.com>
2022-05-20 11:15:12 +0000

    Add　カート一覧ページ作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,25 +2,25 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 
   def create
-    
+
     @cart_items = current_customer.cart_items
     @cart_item = CartItem.new(cart_item_params)
-    
+
     if @cart_items.find_by(item_id: @cart_item.item_id)
       @cart_item = @cart_items.find_by(item_id: @cart_item.item_id)
       @cart_item.amount += @cart_item.amount.to_i
       @cart_item.save
-      
+
     else
       @cart_item.customer_id = current_customer.id
       @cart_item.save
     end
-    
     redirect_to cart_items_path
-    
+
   end
 
   def update
@@ -37,10 +37,11 @@ class Public::CartItemsController < ApplicationController
 
   def destroy_all
     current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
