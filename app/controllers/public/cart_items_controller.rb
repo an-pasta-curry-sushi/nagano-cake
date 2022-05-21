e70d3594d813bfe952be4e427f5526d4ea5acd18
EC2 Default User <kkisobe1213@icloud.com>
2022-05-21 02:23:24 +0000

    Fix cart_items/controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,14 +7,14 @@ class Public::CartItemsController < ApplicationController
   def create
     
     @cart_items = current_customer.cart_items
-    @cart_item = CartItem.new(cart_item_params)
     
-    if @cart_items.find_by(item_id: @cart_item.item_id)
-      @cart_item = @cart_items.find_by(item_id: @cart_item.item_id)
-      @cart_item.amount += @cart_item.amount.to_i
+    
+    if @cart_item = @cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
       @cart_item.save
       
     else
+      @cart_item = CartItem.new(cart_item_params)
       @cart_item.customer_id = current_customer.id
       @cart_item.save
     end
