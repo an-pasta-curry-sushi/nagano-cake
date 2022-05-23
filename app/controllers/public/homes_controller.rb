92acdf78e26796cc0157933408525989876aa9ea
koichi-utebi <102844020+koichi-utebi@users.noreply.github.com>
2022-05-23 15:43:12 +0900

    Update homes_controller.rb

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,5 @@
 class Public::HomesController < Public::ApplicationController
   before_action :authenticate_customer!, except: [:top, :about]
-  before_action :authenticate_admin!, except: [:top, :about]
   
   def top
     @items = Item.latest.limit(4)
