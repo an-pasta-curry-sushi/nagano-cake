f755152dfe4fa41213970978a73f98ab5e95597f
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-23 08:46:02 +0000

    [Update] error,flash message(2)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -29,41 +29,41 @@ class Public::OrdersController < Public::ApplicationController
       @order.name = current_customer.get_full_name(current_customer)
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      
+
     elsif params[:order][:delivery_number] == "1"
-      
+
       if Delivery.find_by(id: params[:order][:delivery_id])
         @order.name = Delivery.find(params[:order][:delivery_id]).name
         @order.postal_code = Delivery.find(params[:order][:delivery_id]).postal_code
         @order.address = Delivery.find(params[:order][:delivery_id]).address
       else
-        
+
         render 'new'
       end
-      
+
     elsif params[:order][:delivery_number] == "2"
       delivery_new = Delivery.new(delivery_params)
       delivery_new.customer_id = current_customer.id
       if delivery_new.save
-        flash[:notice] = '新規お届け先を登録'
+        flash[:notice] = '新規お届け先を登録しました'
       else
-        
+
         render 'new'
-        
+
       end
     else
-      
+
       render 'new'
-      
+
     end
-    
+
   end
 
   def create
-    
+
     @cart_items = current_customer.cart_items
     @order = current_customer.orders.new(order_params)
-    
+
     if @order.save
       @cart_items.each do |cart_item|
         @order_detail = OrderDetail.new
@@ -73,15 +73,15 @@ class Public::OrdersController < Public::ApplicationController
         @order_detail.amount = cart_item.amount
         @order_detail.save
       end
-      
+
     redirect_to thanks_order_path
-    
+
     @cart_items.destroy_all
-    
+
     else
-      
+
       render 'new'
-      
+
     end
   end
 
