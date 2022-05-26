dcd5e87584fffbf16c2e0d5ed31926311a262e8f
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-26 07:03:29 +0000

    Fix public/items views [:index]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < Public::ApplicationController
 
   def index
     @total_items = Item.all
-    @items = Item.latest.page(params[:page])
+    @items = Item.latest.page(params[:page]).per(8)
     @genres = Genre.all
   end
 
