233786753c2742ca14f2b682f954d204136a5b9c
kawa-sho <syogo.wao@gmail.com>
2022-05-26 04:01:55 +0000

    Update ランキングのページネーション

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -12,7 +12,7 @@ class Public::ItemsController < Public::ApplicationController
 
     #ひと月分のランキングページ
     month_order_detail = OrderDetail.where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day)
-    @month_rank_items = Item.find(month_order_detail.group(:item_id).order('count(item_id) desc').pluck(:item_id))
+    @month_rank_items = Item.page(params[:page]).per(8).find(month_order_detail.group(:item_id).order('count(item_id) desc').pluck(:item_id))
   end
 
   def show
