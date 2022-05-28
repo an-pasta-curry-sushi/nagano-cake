dcf360a75caa43e2868418472bebf523402e6ed4
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-28 09:00:29 +0000

    最終決戦

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Customer.create!(
-    email: 'user@user.com',
+Admin.create!(
+    email: 'test@test.com',
     password: 'abc123',
-    last_name: '太郎',
-    first_name: '山田',
-    last_name_easy: 'タロウ',
-    first_name_easy: 'ヤマダ',
-    postal_code: '1000014',
-    address:  '東京都千代田区永田町1-7-1',
-    phone_number: '08012345678',
-    is_active: 'true',
 )
